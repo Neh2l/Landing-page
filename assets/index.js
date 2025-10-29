996c089aa772a561101485394890711860580635
Luffycoder0 <luffycoding0@gmail.com>
2025-10-30 02:32:58 +0300

    AOS edit

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,12 @@
+AOS.init({
+  offset: 120,
+  delay: 0,
+  duration: 400,
+  easing: 'ease',
+  once: false,
+  mirror: false
+});
+
 const menu = document.getElementById('menu');
 const navLeft = document.querySelector('.nav-left');
 menu.addEventListener('click', () => {
