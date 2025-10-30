a5864ce17ca45fd8b74c78cea7996606728b71c6
Neh2l <n40233099@gmail.com>
2025-10-30 10:13:40 +0300

    edit AOS

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,4 @@
-AOS.init({
-  offset: 120,
-  delay: 0,
-  duration: 400,
-  easing: 'ease',
-  once: false,
-  mirror: false
-});
+
 
 const menu = document.getElementById('menu');
 const navLeft = document.querySelector('.nav-left');
